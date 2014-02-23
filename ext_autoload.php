98a7c45dc59ac4643f8fba531fe3badc69c4f65f
Klaus Heuer <post@heuer-illustration.de>
2014-02-23 12:37:14 +0100

    Repro change   version 0.1 -> 0.2

--- ext_autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-$extensionPath = t3lib_extMgm::extPath('projects_and_tasks');
-return array(
-	'tcpdf' => $extensionPath . 'Classes/Utility/Tcpdf/tcpdf.php',
-        'Tx_ProjectsAndTasks_Controller_TodoController' => $extensionPath . 'Classes/Controller/TodoController.php',
-);
-?>
