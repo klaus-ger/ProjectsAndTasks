b034450955b54b4dac787c951d96e362aa05f8c8
Klaus Heuer <post@heuer-illustration.de>
2014-10-17 16:51:58 +0200

    [TASK] Add userright Basics

---
+++ Classes/Domain/Model/Userrights.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace T3developer\ProjectsAndTasks\Domain\Model;
+
+/* * *************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2014 Klaus Heuer <klaus.heuer@t3-developer.com>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+/**
+ *
+ *
+ * @package projects and tasks
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ */
+class Userrights extends \TYPO3\CMS\Extbase\DomainObject\AbstractEntity {
+
+    /**
+     * Right group Titel
+     * @var \string
+     * 
+     */
+    protected $right_Name;
+
+    /**
+     * In Box Menu
+     * @var \int
+     * 
+     */
+    protected $showInboxMenu;
+
+    /**
+     * Project Menu
+     * @var \int
+     * 
+     */
+    protected $showProjectMenu;
+
+    /**
+     * Ticket Menu
+     * @var \int
+     * 
+     */
+    protected $showTicketMenu;
+
+    /**
+     * Times Menu
+     * @var \int
+     * 
+     */
+    protected $showTimeMenu;
+
+    /**
+     * Adress Menu
+     * @var \int
+     * 
+     */
+    protected $showAddressMenu;
+
+    /**
+     * Whiteboard Menu
+     * @var \int
+     * 
+     */
+    protected $showWhiteboardMenu;
+
+    /**
+     * Settings Menu
+     * @var \int
+     * 
+     */
+    protected $showSettingMenu;
+
+    public function getRight_Name() {
+        return $this->right_Name;
+    }
+
+    public function setRight_Name($right_Name) {
+        $this->right_Name = $right_Name;
+    }
+
+    public function getShowInboxMenu() {
+        return $this->showInboxMenu;
+    }
+
+    public function setShowInboxMenu($showInboxMenu) {
+        $this->showInboxMenu = $showInboxMenu;
+    }
+
+    public function getShowProjectMenu() {
+        return $this->showProjectMenu;
+    }
+
+    public function setShowProjectMenu($showProjectMenu) {
+        $this->showProjectMenu = $showProjectMenu;
+    }
+
+    public function getShowTicketMenu() {
+        return $this->showTicketMenu;
+    }
+
+    public function setShowTicketMenu($showTicketMenu) {
+        $this->showTicketMenu = $showTicketMenu;
+    }
+
+    public function getShowTimeMenu() {
+        return $this->showTimeMenu;
+    }
+
+    public function setShowTimeMenu($showTimeMenu) {
+        $this->showTimeMenu = $showTimeMenu;
+    }
+
+    public function getShowAddressMenu() {
+        return $this->showAddressMenu;
+    }
+
+    public function setShowAddressMenu($showAddressMenu) {
+        $this->showAddressMenu = $showAddressMenu;
+    }
+
+    public function getShowWhiteboardMenu() {
+        return $this->showWhiteboardMenu;
+    }
+
+    public function setShowWhiteboardMenu($showWhiteboardMenu) {
+        $this->showWhiteboardMenu = $showWhiteboardMenu;
+    }
+
+    public function getShowSettingMenu() {
+        return $this->showSettingMenu;
+    }
+
+    public function setShowSettingMenu($showSettingMenu) {
+        $this->showSettingMenu = $showSettingMenu;
+    }
+
+}
+
+?>
---
+++ Configuration/TCA/tx_projectsandtasks_domain_model_userrights.php
@@ -0,0 +1,150 @@
+<?php
+
+return array(
+    'ctrl' => array(
+        'title' => 'Userrights',
+        'label' => 'right_name',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'dividers2tabs' => TRUE,
+        'versioningWS' => 2,
+        'versioning_followPages' => TRUE,
+        'origUid' => 't3_origuid',
+        'languageField' => 'sys_language_uid',
+        'transOrigPointerField' => 'l18n_parent',
+        'transOrigDiffSourceField' => 'l18n_diffsource',
+        'delete' => 'deleted',
+        'enablecolumns' => array(
+            'disabled' => 'hidden',
+        ),
+        'searchFields' => 'bc_title',
+        'iconfile' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath('projects_and_tasks') . 'Resources/Public/Icons/tableicon.gif'
+    ),
+    'interface' => array(
+        'showRecordFieldList' => '    right_name
+                                    '
+    ),
+    'types' => array(
+        '1' => array('showitem' => '  right_name
+                                     
+                                     ')
+    ),
+    'palettes' => array(
+        '1' => array('showitem' => '')
+    ),
+    'columns' => array(
+        'sys_language_uid' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.php:LGL.language',
+            'config' => array(
+                'type' => 'select',
+                'foreign_table' => 'sys_language',
+                'foreign_table_where' => 'ORDER BY sys_language.title',
+                'items' => array(
+                    array('LLL:EXT:lang/locallang_general.php:LGL.allLanguages', -1),
+                    array('LLL:EXT:lang/locallang_general.php:LGL.default_value', 0)
+                )
+            )
+        ),
+        'l18n_parent' => array(
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.php:LGL.l18n_parent',
+            'config' => array(
+                'type' => 'select',
+                'items' => array(
+                    array('', 0),
+                ),
+                'foreign_table' => 'tx_projectsandtasks_domain_model_userrights',
+                'foreign_table_where' => 'AND tx_projectsandtasks_domain_model_userrights.uid=###REC_FIELD_l18n_parent### AND tx_projectsandtasks_domain_model_userrights.sys_language_uid IN (-1,0)',
+            )
+        ),
+        'l18n_diffsource' => array(
+            'config' => array(
+                'type' => 'passthrough')
+        ),
+        't3ver_label' => array(
+            'displayCond' => 'FIELD:t3ver_label:REQ:true',
+            'label' => 'LLL:EXT:lang/locallang_general.php:LGL.versionLabel',
+            'config' => array(
+                'type' => 'none',
+                'cols' => 27
+            )
+        ),
+        'hidden' => array(
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xml:LGL.hidden',
+            'config' => array(
+                'type' => 'check'
+            )
+        ),
+        'crdate' => array(
+            'exclude' => 0,
+            'label' => 'crdate',
+            'config' => array(
+                'type' => 'input',
+                'size' => 30,
+                'eval' => 'trim',
+            )
+        ),
+        'right_name' => array(
+            'exclude' => 0,
+            'label' => 'Right Group',
+            'config' => array(
+                'type' => 'text',
+                'size' => 30,
+            )
+        ),
+        'show_inbox_menu' => array(
+            'exclude' => 0,
+            'label' => 'Date',
+            'config' => array(
+                'type' => 'check'
+            )
+        ),
+        'show_project_menu' => array(
+            'exclude' => 0,
+            'label' => 'Date',
+            'config' => array(
+                'type' => 'check'
+            )
+        ),
+        'show_ticket_menu' => array(
+            'exclude' => 0,
+            'label' => 'Date',
+            'config' => array(
+                'type' => 'check'
+            )
+        ),
+        'show_time_menu' => array(
+            'exclude' => 0,
+            'label' => 'Date',
+            'config' => array(
+                'type' => 'check'
+            )
+        ),
+        'show_address_menu' => array(
+            'exclude' => 0,
+            'label' => 'Date',
+            'config' => array(
+                'type' => 'check'
+            )
+        ),
+        'show_whiteboard_menu' => array(
+            'exclude' => 0,
+            'label' => 'Date',
+            'config' => array(
+                'type' => 'check'
+            )
+        ),
+        'show_setting_menu' => array(
+            'exclude' => 0,
+            'label' => 'Date',
+            'config' => array(
+                'type' => 'check'
+            )
+        ),
+        
+    ),
+);
+?>
